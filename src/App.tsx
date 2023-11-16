0ef0b4943d9b97846da06a15ce8f2b99906d3643
ori_reiter <ori.reiter@gmail.com>
2023-11-16 22:51:24 +0100

    reformat all files according to new rule

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@ import '@mantine/core/styles.css';
 import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 
 import { Layout } from './pages/layout/Layout.page';
-import { Login } from './pages/login/Login.page'
+import { Login } from './pages/login/Login.page';
 
 function App() {
-  return (
-    <>
-      <ColorSchemeScript defaultColorScheme="auto" />
-      <MantineProvider defaultColorScheme="auto">
-        <Layout>
-          <Login />
-        </Layout>
-      </ MantineProvider>
-    </>
-  )
+    return (
+        <>
+            <ColorSchemeScript defaultColorScheme='auto' />
+            <MantineProvider defaultColorScheme='auto'>
+                <Layout>
+                    <Login />
+                </Layout>
+            </ MantineProvider>
+        </>
+    );
 }
 
-export default App
+export default App;
