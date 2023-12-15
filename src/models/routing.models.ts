b5870e3c2f4013e7f58b4e82910a5bbe0d737127
ori_reiter <ori.reiter@gmail.com>
2023-12-15 17:07:38 +0200

    pass routes into navbar

--- src/models/routing.models.ts
+++ src/models/routing.models.ts
@@ -1,7 +1,11 @@
 import { JSX } from 'react';
 
+import { IconEnum } from './common.models';
+
+
 export interface Route {
     name?: string;
+    iconEnum?: IconEnum
     path: string;
     element: JSX.Element;
 }
--- src/pages/layout/components/Navbar.tsx
+++ src/pages/layout/components/Navbar.tsx
@@ -1,17 +1,19 @@
-import { AppShell, Skeleton, ScrollArea, Group, ActionIcon, Tooltip, Loader } from '@mantine/core';
-import { IconLogout } from '@tabler/icons-react';
+import { AppShell, ScrollArea, Group, ActionIcon, Tooltip, Loader, NavLink, Container, Stack, Paper } from '@mantine/core';
 
 import { ThemeToggle } from '../../../components/ThemeToggle';
 import { useLogout, useAuthSession } from '../../../hooks/authentication.hooks';
+import { Route } from '../../../models/routing.models';
+import { Icon } from '../../../components/Icon';
+import { IconEnum, ColorEnum } from '../../../models/common.models';
 
 
-function NavbarFooter(props: {closeNavbar: () => void}) {
+function NavbarFooter(props: { closeNavbar: () => void }) {
     const { isAuthenticated } = useAuthSession();
     const { logout, isLoading } = useLogout();
 
-    let logoutIcon = null;
+    let logoutButton = null;
     if(isAuthenticated) {
-        logoutIcon = (<ActionIcon onClick={() => {
+        logoutButton = (<ActionIcon onClick={() => {
             void logout();
             props.closeNavbar();
         }}
@@ -22,7 +24,9 @@ function NavbarFooter(props: {closeNavbar: () => void}) {
         disabled={isLoading}
         >
             <Tooltip label='log out' offset={0} position='top'>
-                {isLoading ? <Loader /> : <IconLogout stroke={1.5} />}
+                <Container>
+                    {isLoading ? <Loader /> : <Icon iconEnum={IconEnum.LOGOUT} />}
+                </Container>
             </Tooltip>
         </ActionIcon>);
     } 
@@ -30,20 +34,39 @@ function NavbarFooter(props: {closeNavbar: () => void}) {
     return (
         <Group justify='space-between' gap='sm'>
             <ThemeToggle />
-            {logoutIcon}
+            {logoutButton}
         </Group>);
 }
 
 
-export function Navbar(props: {closeNavbar: () => void}) {
+export function Navbar(props: { closeNavbar: () => void, routes: Route[]}) {
+    const routesComponents =  [];
+    
+    for (const route of props.routes) {
+        if (!route.name) {
+            continue;
+        }
+
+        routesComponents.push(
+            <Paper key={route.path} withBorder>
+                <NavLink
+                    href={route.path}
+                    label={route.name}
+                    onClick={() => {
+                        props.closeNavbar();
+                    }}
+                    leftSection={route.iconEnum ? <Icon iconEnum={route.iconEnum} color={ColorEnum.BLUE}/> : null}
+                />
+            </Paper>
+        );
+    }
+
     return (
         <>
             <AppShell.Section grow my='md' component={ScrollArea}>
-                {Array(15)
-                    .fill(0)
-                    .map((_, index) => (
-                        <Skeleton key={index} h={'2rem'} mt='sm' />
-                    ))}
+                <Stack gap='xs'>
+                    {routesComponents}
+                </Stack>
             </AppShell.Section>
             <AppShell.Section>
                 <NavbarFooter closeNavbar={props.closeNavbar}/>
