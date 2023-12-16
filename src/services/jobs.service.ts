cb44af51938f0c94408308c555b68a25c0d9448b
ori_reiter <ori.reiter@gmail.com>
2023-12-17 01:09:15 +0200

    adding new job - connected to backend

--- src/services/jobs.service.ts
+++ src/services/jobs.service.ts
@@ -1,7 +1,43 @@
-import { NewJob } from '../models/jobs.models';
-import { NotImplementedError } from '../models/errors.models';
+import { supabase } from '../thirdParties/supabase';
+import { NewJob, ExistingJob } from '../models/jobs.models';
+
+
+const jobsTable = supabase.from('jobs');
+
 
 
 export async function saveNewJob(job: NewJob) {
-    throw new NotImplementedError(`${saveNewJob.name}`);
+    const remoteJob: Record<string, string | number> = {
+        name: job.name,
+        salary_amount: job.salaryAmount,
+        salary_frequency: job.salaryFrequency,
+        salary_currency: job.salaryCurrency,
+    };
+
+    if (job.address) {
+        remoteJob.address = job.address;
+    }
+
+    if (job.note) {
+        remoteJob.note = job.note;
+    }
+
+
+    const { data, error } = await jobsTable.insert([remoteJob,]).select();
+
+    if (error) { 
+        throw new Error('failed to save job');
+    }
+
+    const savedJob = data[0];
+    `address: null
+    created_at: "2023-12-16T22:53:47.924501+00:00"
+    id: 3
+    name: "test"
+    note: null
+    salary_amount: 1
+    salary_currency: "NIS"
+    salary_frequency: "hourly"
+    user_id: "5694c6ef-edbf-4cbc-ac12-d79a31808c01"`;
+    return savedJob;
 }
