bd058ba6cc699d9c6084db0b9966b6097f076c8b
ori_reiter <ori.reiter@gmail.com>
2024-10-16 18:44:55 +0300

    remove supabase env vars

--- src/thirdParties/supabase.ts
+++ src/thirdParties/supabase.ts
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Create a single supabase client for interacting with your database
-const url = 'https://tsglwcjcwadmlagrugrd.supabase.co';
-const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzZ2x3Y2pjd2FkbWxhZ3J1Z3JkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIxMzc2NjIsImV4cCI6MjAxNzcxMzY2Mn0.hkRLh0g-Ytb24EModpdndjInrYIsyu9W2NaiaJWPafU';
+const url = import.meta.env.VITE_SUPABASE_URL as string ?? '';
+const key = import.meta.env.VITE_SUPABASE_ANON_KEY as string ?? '';
 
 export const supabase = createClient(url, key);
